6ccf14ece94ea0770cc4980d31a48b2374dbe1d7
Axellerosh <lubi.axellerosh@gmail.com>
2025-03-07 22:35:15 +0800

    components of the sidebar

---
+++ components/Library.tsx
@@ -0,0 +1,35 @@
+import { AiOutlinePlus } from '@node_modules/react-icons/ai'
+import { TbPlaylist } from '@node_modules/react-icons/tb'
+import React from 'react'
+
+type Props = {}
+
+export default function Library({}: Props) {
+  //handles uploads of songs
+  const onClick = () => {
+
+  }
+
+  return (
+    <div className='flex flex-col'>
+      <div className="flex items-center justify-between px-5 pt-4">
+        <div className="inline-flex items-center gap-x-2 ">
+          <TbPlaylist className='text-neutral-400' size={26}/>
+          <p className='text-neutral-400 font-medium text-base'>Your Library</p>
+        </div>
+        <AiOutlinePlus onClick={onClick}
+          size={20}
+          className='
+             text-neutral-400
+             cursor-pointer
+             hover:text-white
+             transition
+          '
+        />
+      </div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        List of Songs!
+      </div>
+    </div>
+  )
+}
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { BiSearch } from "@node_modules/react-icons/bi";
 import { HiHome } from "@node_modules/react-icons/hi";
 import Box from "./Box"
 import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -42,7 +43,7 @@ const Sidebar = ({children}: SidebarProps) => {
                     </div>
                 </Box>
                 <Box className="overflow-y-auto h-full">
-                    Song Library
+                    <Library />
                 </Box>
             </div>
             <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
