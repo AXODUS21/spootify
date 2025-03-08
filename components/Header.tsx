75993ace84863ce63ca7f731a5d4b0fcfe79b3eb
Axellerosh <lubi.axellerosh@gmail.com>
2025-03-09 01:08:42 +0800

    adding log out function

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,10 @@ import { BiSearch } from '@node_modules/react-icons/bi';
 import { HiHome } from '@node_modules/react-icons/hi';
 import { RxCaretLeft, RxCaretRight } from '@node_modules/react-icons/rx';
 import { twMerge } from '@node_modules/tailwind-merge';
+import { useSupabaseClient } from '@node_modules/@supabase/auth-helpers-react/dist';
+import { useUser } from '@hooks/useUser';
+import { FaUserAlt } from '@node_modules/react-icons/fa';
+import { toast } from 'react-hot-toast';
 import React from 'react'
 import Button from './Button';
 import useAuthModal from '@hooks/useAuthModal';
@@ -16,8 +20,20 @@ type Props = {
 export default function Header({ children, className}: Props) {
     const router = useRouter();
     const {onOpen} = useAuthModal();
-    const handleLogOut = () => {
+    const supabaseClient = useSupabaseClient();
+    const { user } = useUser();
 
+    const handleLogOut = async () => {
+        const {error} = await supabaseClient.auth.signOut();
+        
+        //TODO : Reset any playing songs
+        router.refresh();
+        
+        if(error) {
+            toast.error(error.message);
+        } else {
+            toast.success('Logged out!');
+        }
     }
     
   return (
@@ -43,6 +59,22 @@ export default function Header({ children, className}: Props) {
             </div>
 
             <div className='flex justify-between items-center gap-x-4'>
+                {user ? (
+                    <div className="flex gap-x-4 items-center">
+                        <Button 
+                            onClick={handleLogOut}
+                            className='bg-white px-6 py-2'
+                        >
+                            Logout
+                        </Button>
+                        <Button
+                            onClick={() => router.push('/account')}
+                            className='bg-white px-3 py-3 '
+                        >
+                            <FaUserAlt />
+                        </Button>
+                    </div>
+                ): (
                 <>
                   <div className="">
                     <Button 
@@ -70,6 +102,7 @@ export default function Header({ children, className}: Props) {
                     </Button>
                   </div>
                 </>
+                )}
             </div>
         </div>
         {children}
