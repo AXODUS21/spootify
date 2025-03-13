9859461e8794ae40a6897d25bcdd64add85b74ed
Axellerosh <lubi.axellerosh@gmail.com>
2025-03-13 19:39:58 +0800

    fixing deployment issues

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,39 +1,38 @@
-import getSongsByTitle from '@actions/getSongByTitle';
-import Header from '@components/Header';
-import SearchInput from '@components/SearchInput';
-import React from 'react'
-import SearchContent from './SearchContent';
+import getSongsByTitle from "@actions/getSongByTitle";
+import Header from "@components/Header";
+import SearchInput from "@components/SearchInput";
+import SearchContent from "./SearchContent";
 
-interface Props {
-  searchParams: Record<string, string | undefined>;
-}
 export const revalidate = 0;
 
-const Search = async ({searchParams}: Props) => {
-    const title = searchParams.title || '';
-    const songs = await getSongsByTitle(title);
+const Search = async ({
+  searchParams,
+}: {
+  searchParams: Record<string, string | undefined>;
+}) => {
+  const title = searchParams?.title || ""; // Ensure title is always a string
+  const songs = await getSongsByTitle(title);
 
   return (
-    <div className='
-        bg-neutral-900
-        rounded-lg
-        h-full
-        w-full
-        overflow-hidden
-        overflow-y-auto
-    '>
-        <Header className='from-bg-neutral-900'>
-            <div className="mb-2 flex flex-col gap-y-6">
-                <h1 className='text-white text-3xl font-semibold'>
-                    Search
-                </h1>
-
-                <SearchInput />
-            </div>
-        </Header>
-        <SearchContent songs={songs}/>
+    <div
+      className="
+            bg-neutral-900
+            rounded-lg
+            h-full
+            w-full
+            overflow-hidden
+            overflow-y-auto
+        "
+    >
+      <Header className="from-bg-neutral-900">
+        <div className="mb-2 flex flex-col gap-y-6">
+          <h1 className="text-white text-3xl font-semibold">Search</h1>
+          <SearchInput />
+        </div>
+      </Header>
+      <SearchContent songs={songs} />
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
